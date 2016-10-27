3f54a09ca9a35a9fee961d6f07d5d6245e600d80
Henry Vonfire <marcoumpierrez@gmail.com>
2016-10-27 21:31:19 +0200

    algorithm 2

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ function bar(){
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
+
+function factorialize(num) {
+  let result = 1;
+  for (let i=1;i<=num;i++){
+    result*=i;
+  }
+  return result;
+}
--- tests.js
+++ tests.js
@@ -13,3 +13,11 @@ test('Reverse a String', function(assert) {
    assert.equal(reverseString('hello'), 'olleh', 'reverseString("hello") should become "olleh"');
    assert.equal(reverseString('Greetings from Earth'), 'htraE morf sgniteerG', 'reverseString("Greetings from Earth") should return "htraE morf sgniteerG"');
 });
+
+test('Factorialize a Number', function(assert) {
+   assert.equal(typeof factorialize(5), 'number', 'factorialize(5) should return a number');
+   assert.equal(factorialize(5), 120, 'factorialize(5) should return 120');
+   assert.equal(factorialize(10), 3628800, 'factorialize(10) should return 3628800');
+   assert.equal(factorialize(20), 2432902008176640000, 'factorialize(20) should return 2432902008176640000');
+   assert.equal(factorialize(0), 1, 'factorialize(0) should return 1');
+});
