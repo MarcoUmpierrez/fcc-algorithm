124ffb8cde70c454568c8b316e6a332148c7c1d6
Henry Vonfire <marcoumpierrez@gmail.com>
2016-10-27 21:24:00 +0200

    algorithm 1

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 function bar(){
   return true;
 };
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
--- tests.js
+++ tests.js
@@ -7,3 +7,9 @@ test('Testing the test suit', function(assert) {
 test('Testing the functions of the main.js file', function(assert) {
    assert.ok(bar(),'function is gotten and returns true');
 });
+
+test('Reverse a String', function(assert) {
+   assert.equal(typeof reverseString('hello'), 'string', 'reverseString("hello") should return a string');
+   assert.equal(reverseString('hello'), 'olleh', 'reverseString("hello") should become "olleh"');
+   assert.equal(reverseString('Greetings from Earth'), 'htraE morf sgniteerG', 'reverseString("Greetings from Earth") should return "htraE morf sgniteerG"');
+});
