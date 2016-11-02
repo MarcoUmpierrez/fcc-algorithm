10972e6724f219bf13c111092b6672d2feabc488
Henry Vonfire <marcoumpierrez@gmail.com>
2016-11-02 18:45:13 +0100

    algorithms 6, 7 and 8

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
-function bar(){
+function bar() {
   return true;
 };
 
 function reverseString(str) {
-  return str.split("").reverse().join("");
+  return str.split('').reverse().join('');
 }
 
 function factorialize(num) {
   let result = 1;
-  for (let i=1;i<=num;i++){
-    result*=i;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
   }
   return result;
 }
 
 function palindrome(str) {
-  let cleanStr = str.replace(/[^A-Za-z0-9]+/g, '').toLowerCase();
+  let cleanStr = str.replace(/[^A-Za-z0-9]+/g, "").toLowerCase();
   return cleanStr.localeCompare(cleanStr.split("").reverse().join("")) === 0;
+}
+
+function findLongestWord(str) {
+  let strArray = str.split(' ');
+  let maxLength = 0;
+  strArray.filter(word => {
+    if (word.length > maxLength){
+      maxLength = word.length;
+    }
+  });
+  return maxLength;
+}
+
+function titleCase(str) {
+  let strArray = str.split(' ');
+  let result = strArray.map(word => {
+    return word.replace(/([A-Za-z])(.+|$)/, (match, p1, p2) => {
+        return p1.toUpperCase() + p2.toLowerCase();
+    });
+  });
+  return result.join(' ');
+}
+
+function largestOfFour(arr) {
+  let result = [];
+  arr.forEach(subArr => {
+    result.push(subArr.reduce((a, b) => { 
+          return a > b? a : b;
+        }));
+  });
+  return result;
 }
--- tests.js
+++ tests.js
@@ -37,3 +37,25 @@ test('Check for Palindromes', function(assert) {
    assert.ok(palindrome("0_0 (: /-\ :) 0-0"), 'palindrome("0_0 (: /-\ :) 0-0") should return true');
    assert.notOk(palindrome("five|\_/|four"), 'palindrome("five|\_/|four") should return false');
 });
+
+test('Find the Longest Word in a String', function(assert) {
+   assert.equal(typeof findLongestWord("The quick brown fox jumped over the lazy dog"), 'number', 'findLongestWord("The quick brown fox jumped over the lazy dog") should return a number');
+   assert.equal(findLongestWord("The quick brown fox jumped over the lazy dog"), 6, 'findLongestWord("The quick brown fox jumped over the lazy dog") should return 6');
+   assert.equal(findLongestWord("May the force be with you"), 5, 'findLongestWord("May the force be with you") should return 5');
+   assert.equal(findLongestWord("Google do a barrel roll"), 6, 'findLongestWord("Google do a barrel roll") should return 6');
+   assert.equal(findLongestWord("What is the average airspeed velocity of an unladen swallow"), 8, 'findLongestWord("What is the average airspeed velocity of an unladen swallow") should return 8');
+   assert.equal(findLongestWord("What if we try a super-long word such as otorhinolaryngology"), 19, 'findLongestWord("What if we try a super-long word such as otorhinolaryngology") should return 19');
+});
+
+test('Title Case a Sentence', function(assert) {
+   assert.equal(typeof titleCase("I'm a little tea pot"), 'string', 'titleCase("I\'m a little tea pot") should return a string');
+   assert.equal(titleCase("I\'m a little tea pot"), "I'm A Little Tea Pot", 'titleCase("I\'m a little tea pot") should return "I\'m A Little Tea Pot"');
+   assert.equal(titleCase("sHoRt AnD sToUt"), "Short And Stout", 'titleCase("sHoRt AnD sToUt") should return "Short And Stout"');
+   assert.equal(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"), "Here Is My Handle Here Is My Spout", 'titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return "Here Is My Handle Here Is My Spout"');
+});
+
+test('Return Largest Numbers in Arrays', function(assert) {
+   assert.ok(Array.isArray(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])), 'largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array');
+   assert.deepEqual(largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]), [27,5,39,1001], 'largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27,5,39,1001]');
+   assert.deepEqual(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]), [9, 35, 97, 1000000], 'largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000]');
+});
